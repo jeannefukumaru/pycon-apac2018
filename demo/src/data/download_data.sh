8156b34d8e3bf702e7bc726729e440a3c1ae84e0
Jeanne Choo <jchoo@aisingapore.org>
2018-05-28 16:55:35 +0800

    added scripts to download data

---
+++ demo/src/data/download_data.sh
@@ -0,0 +1,3 @@
+#!usr/bin/bash
+curl http://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data -o ../../data/raw/housing.txt
+curl http://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.names -o ../../data/raw/housing-metadata.txt
